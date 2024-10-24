dd6f969e589c16b4c2e5bfec96ceae38f9e038bb
Sleepy Rae <67649181+sleepyexe@users.noreply.github.com>
2024-10-25 03:17:25 +0700

    chore(manifest): Dependencies and provide

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,4 +2,16 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'rwx_core'
+description 'Core resource for rwx framework'
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+version '0.0.1'
+
+dependencies {
+    '/server:7290',
+    '/onesync',
+    'ox_lib',
+    'oxmysql'
+}
+
+provide 'es_extended'
